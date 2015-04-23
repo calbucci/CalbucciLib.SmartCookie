54827a03c0995742ab8a50de0560820a69642261
Marcelo Calbucci <marcelo@calbucci.com>
2015-04-23 12:54:35 -0700

    Nuget package

--- src/SmartCookie/Properties/AssemblyInfo.cs
+++ src/SmartCookie/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SmartCookie")]
+[assembly: AssemblyTitle("CalbucciLib.SmartCookie")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("SmartCookie")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCompany("Marcelo Calbucci")]
+[assembly: AssemblyProduct("CalbucciLib.SmartCookie")]
+[assembly: AssemblyCopyright("Copyright © Marcelo Calbucci 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
